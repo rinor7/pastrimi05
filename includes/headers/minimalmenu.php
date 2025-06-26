4a911ccbbce01f90317103de1bda26938b6a7eba
Rinor <rinorsahiti@hennessey.com>
2025-06-26 22:05:25 +0200

    publish

--- includes/headers/minimalmenu.php
+++ includes/headers/minimalmenu.php
@@ -11,7 +11,7 @@
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!-- Change this to "noindex, nofollow" when you go live -->
+
     <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
     <!-- Open Graph Description (for social media platforms like Facebook, LinkedIn, etc.) -->
     <?php 
